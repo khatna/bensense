1c1d53785781a3c49a1318a5f77703dbdc3a17c8
Khatna Bold <bold@princeton.edu>
2018-10-23 05:07:35 +0000

    grab simmons id successfully

--- app.js
+++ app.js
@@ -1,22 +1,40 @@
 var rp = require("request-promise");
 
+// JSONs to be parsed
 var year = new Date().getFullYear();
 const allPlayers = { 
 	uri: "http://data.nba.net/10s/prod/v1/"+ year +"/players.json",
 	json: true
 };
 
-var ben;
+
+
+// Ben Simmons' ID
+var ben = null;
 
 // fetch json file for all players, and grab Ben Simmons's ID
 rp(allPlayers)
 	.then(function(json) {
-		ben = grabSimmonsId(json.league);
+		ben = grabSimmonsId(json.league.standard);
+		
+		if (!ben) {
+			console.log("Ben Simmons not found!");
+		} else {
+			console.log(ben);
+		}
 	})
 	.catch(function(err) {
 		console.log(err);
 	});
 
+
+
+// grabs Ben Simmons's ID if found
 function grabSimmonsId(players) {
-	// TODO
+	players.forEach(function(player) {
+		if (player.firstName === "Ben" && player.lastName === "Simmons") {
+			ben = player.personId;
+		}	
+	});
+	return ben;
 }
